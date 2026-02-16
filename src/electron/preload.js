38ce94f1276a616cdd95a522812b62468ae5dd19
Seb-Jesh <sebastian.israel.2015@gmail.com>
2026-02-16 11:54:10 +0200

    Remove deleted files

--- src/electron/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  selectFile: () => ipcRenderer.invoke('select-file'),
-  extractData: (filePath) => ipcRenderer.invoke('extract-data', filePath),
-});
