bb90d57bf7634dafdbd4eb07fd304a216689260e
Seb-Jesh <sebastian.israel.2015@gmail.com>
2026-02-16 11:51:02 +0200

    Remove deleted files

--- src/app.js
+++ src/app.js
@@ -18,7 +18,6 @@ const port = process.env.PORT || 3000
 
 app.set('view engine', 'hbs')
 
-// const multer = require('multer');
 import multer from "multer";
 
 // Set up storage for uploaded files
@@ -39,7 +38,6 @@ const storage = multer.diskStorage({
         cb(undefined, true)
     }
 });
-// const storage = multer.memoryStorage()
 
 const upload = multer({ storage: storage });
 
@@ -58,10 +56,7 @@ app.get('', (_, res) => {
 app.post('/upload', upload.single('filename'), async (req,res) => {    
     // Read & Handle the uploaded file
     const file = reader.readFile(req.file.path)
-    // csvFile = req.file.path.split('/')[1].split('.')[0]
     const sheets = file.SheetNames
-    // const file = reader.read(req.file.buffer, { type: 'buffer' });
-    // const sheets = file.SheetNames
 
     if(!sheets.includes("upload")) {
         res.render('download', {
@@ -87,38 +82,6 @@ app.post('/upload', upload.single('filename'), async (req,res) => {
             }
             dataArray.push(newObj)
         });
-        // Filter out invalid rows (Missing amount)
-        // const rows_with_amount = dataArray.filter(item => item.amount !== undefined && item.amount > 0)
-        // Filter out invalid rows (Missing service)
-        // const rows_with_service = rows_with_amount.filter(item => item.service !== undefined)
-        // Filter out invalid rows (Missing phone number)
-        // const rows_with_phone = rows_with_service.filter(item => item.phone !== undefined)
-        // const rows_with_phone = dataArray.filter(item => item.amount !== undefined && item.amount > 0 && item.service !== undefined && item.phone !== undefined)
-        // rows_with_phone.forEach((item) => { 
-        //     item.amount = validate.sanitizeAmount(item.amount); 
-        //     const validPrefix = ['097', '096', '095', '077', '076', '075', '021', '057', '056', '055']
-        //     if(item.service.toLowerCase() === "airtime" || item.service.toLowerCase() === "data" || item.service.toLowerCase() === "momo") {
-        //         item.phone = validate.sanitizePhone(item.phone);
-        //         if(validPrefix.indexOf(item.phone.substring(0,3)) < 0 || item.phone.length !== 10) {
-        //             item.status = "Invalid row";
-        //         }
-        //         item.ServiceProvider = validate.product(item.phone)
-        //     } 
-        //     if(item.service.toLowerCase() === "liquid") {
-        //         item.phone = validate.sanitizeLiquid(item.phone);
-        //         if(!item.phone.startsWith('260630') || item.phone.length !== 12) {
-        //             item.status = "Invalid row";
-        //         }
-        //     } 
-        //     if(item.service.toLowerCase() === "zesco") {
-        //         item.phone = validate.sanitizeZesco(item.phone);
-        //         if(item.phone.length !== 11)
-        //         item.status = "Invalid row";
-        //     }    
-        //     if(item.amount <= 0) {
-        //         item.status = "Invalid row";
-        //     }      
-        // });
 
         const rows_with_phone = dataArray.filter(item => item.amount !== undefined && item.amount > 0 && item.phone !== undefined)
         rows_with_phone.forEach((item) => { 
@@ -136,10 +99,6 @@ app.post('/upload', upload.single('filename'), async (req,res) => {
        // Construct arrays for each service
         const valid_rows = rows_with_phone.filter(row => row.status !== "Invalid row");
         // airtime_rows = rows_with_phone.filter(row => row.service.toLowerCase() === "airtime");
-        // data_rows = rows_with_phone.filter(row => row.service.toLowerCase() === "data");
-        // liquid_rows = rows_with_phone.filter(row => row.service.toLowerCase() === "liquid");
-        // momo_rows = rows_with_phone.filter(row => row.service.toLowerCase() === "momo");
-        // zesco_rows = rows_with_phone.filter(row => row.service.toLowerCase() === "zesco");
     
         // const rows_paysmart = valid_rows.map(row => ({
         //     reference: row.name, 
@@ -157,40 +116,14 @@ app.post('/upload', upload.single('filename'), async (req,res) => {
         count = rows_with_phone.length
         sum = rows_with_phone.reduce((accumulator, currentValue) => accumulator + currentValue.amount, 0,);
         airtime_sum = rows_with_phone.reduce((accumulator, currentValue) => accumulator + currentValue.amount, 0,);
-        // data_sum = data_rows.reduce((accumulator, currentValue) => accumulator + currentValue.amount, 0,);
-        // liquid_sum = liquid_rows.reduce((accumulator, currentValue) => accumulator + currentValue.amount, 0,);
-        // momo_sum = momo_rows.reduce((accumulator, currentValue) => accumulator + currentValue.amount, 0,);
-        // zesco_sum = zesco_rows.reduce((accumulator, currentValue) => accumulator + currentValue.amount, 0,);
     
         valid_rows_count = valid_rows.length
         sum_valid = valid_rows.reduce((accumulator, currentValue) => accumulator + currentValue.amount,
         0,);
     
-        // // Printing data 
-        // // console.log(data)
-        // // const workbook = reader.utils.book_new();
-        // // reader.utils.book_append_sheet(workbook, worksheet, "Airtime");
-        // // reader.writeFile(workbook, "upload.xlsx");
-        // // var output_file_name = "../../../../Downloads/batch.csv";
-        // // Extract Data (create a workbook object from the table)
-    
-        // // Create csv file for paysmart website
-        // const worksheet = reader.utils.json_to_sheet(rows_paysmart);
-        // var paysmart_file_name = `../csv_downloads/${csvFile}.csv`
-        // var stream = reader.stream.to_csv(worksheet);
-        // stream.pipe(fs.createWriteStream(paysmart_file_name));
-    
         // Create excel file for 543 website
         worksheet = reader.utils.json_to_sheet(rows_cgrate);
-        // var cgrate_file_name = `../cgrate_lists/${csvFile}.xlsx`
-        // const cgrate_file_name = `../cgrate_lists/${req.file.originalname}`;
         filename = path.join(os.homedir(), 'cgrate_lists', req.file.originalname)
-        // var stream = reader.stream.to_csv(worksheet2);
-        // stream.pipe(fs.createWriteStream(cgrate_file_name)); 
-
-        // workbook = reader.utils.book_new();
-        // reader.utils.book_append_sheet(workbook, worksheet, 'cgrate');
-        // reader.writeFileXLSX(workbook, filename);
         
         // Create pdf
         clientDetails = companyData[0]
@@ -239,10 +172,6 @@ app.get('/quote', (_,res) => {
         description: 'your quotation has been downloaded'
     })
 });
-
-// app.get('/list', (_,res) => {
-//     res.render('myList', {airtime: airtime_rows, data: data_rows, liquid: liquid_rows, momo: momo_rows, zesco: zesco_rows, count, valid_rows_count, sum, airtime_sum, data_sum, liquid_sum, momo_sum, zesco_sum, sum_valid});
-// });
 
 app.get('/list', (_,res) => {
     res.render('myList', {airtime: rows_with_phone, data: data_rows, liquid: liquid_rows, momo: momo_rows, zesco: zesco_rows, count, valid_rows_count, sum, airtime_sum, data_sum, liquid_sum, momo_sum, zesco_sum, sum_valid});
